553a93fe3a0ccb9b33def0e79c22ff70501287fa
Juan Cruz Viotti <jv@jviotti.com>
2018-06-25 18:59:14 -0400

    repo: initial commit
    Change-type: patch
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ README.md
@@ -0,0 +1,220 @@
+skhema
+======
+
+> JSON Schema utility collection
+
+[![Current Release](https://img.shields.io/npm/v/skhema.svg?style=flat-square)](https://npmjs.com/package/skhema)
+[![License](https://img.shields.io/npm/l/skhema.svg?style=flat-square)](https://npmjs.com/package/skhema)
+[![Downloads](https://img.shields.io/npm/dm/skhema.svg?style=flat-square)](https://npmjs.com/package/skhema)
+[![Dependency status](https://img.shields.io/david/resin-io-modules/skhema.svg?style=flat-square)](https://david-dm.org/resin-io-modules/skhema)
+
+Installation
+------------
+
+Install `skhema` by running:
+
+```sh
+$ npm install --save skhema
+```
+
+Documentation
+-------------
+
+
+* [skhema](#module_skhema)
+    * [.SchemaMismatch](#module_skhema.SchemaMismatch) : <code>Error</code>
+    * [.IncompatibleSchemas](#module_skhema.IncompatibleSchemas) : <code>Error</code>
+    * [.match(schema, object)](#module_skhema.match) ⇒ <code>Object</code>
+    * [.isValid(schema, object)](#module_skhema.isValid) ⇒ <code>Boolean</code>
+    * [.validate(schema, object)](#module_skhema.validate)
+    * [.merge(schemas)](#module_skhema.merge) ⇒ <code>Object</code>
+    * [.filter(schema, object, [options])](#module_skhema.filter) ⇒ <code>Object</code> \| <code>Null</code>
+
+<a name="module_skhema.SchemaMismatch"></a>
+
+### skhema.SchemaMismatch : <code>Error</code>
+**Kind**: static property of [<code>skhema</code>](#module_skhema)  
+**Summary**: Schema mismatch error  
+**Access**: public  
+<a name="module_skhema.IncompatibleSchemas"></a>
+
+### skhema.IncompatibleSchemas : <code>Error</code>
+**Kind**: static property of [<code>skhema</code>](#module_skhema)  
+**Summary**: Incompatible schemas error  
+**Access**: public  
+<a name="module_skhema.match"></a>
+
+### skhema.match(schema, object) ⇒ <code>Object</code>
+**Kind**: static method of [<code>skhema</code>](#module_skhema)  
+**Summary**: Match an object against a schema  
+**Returns**: <code>Object</code> - results  
+**Access**: public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| schema | <code>Object</code> | JSON schema |
+| object | <code>Object</code> | object |
+
+**Example**  
+```js
+const results = skhema.match({
+	 type: 'object'
+}, {
+	 foo: 'bar'
+})
+
+if (!results.valid) {
+	 for (const error of results.errors) {
+		 console.error(error)
+	 }
+}
+```
+<a name="module_skhema.isValid"></a>
+
+### skhema.isValid(schema, object) ⇒ <code>Boolean</code>
+This is a shorthand function for `.match()` which can be used
+if the caller is not interested in the actual error messages.
+
+**Kind**: static method of [<code>skhema</code>](#module_skhema)  
+**Summary**: Check if an object matches a schema  
+**Returns**: <code>Boolean</code> - whether the object matches the schema  
+**Access**: public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| schema | <code>Object</code> | JSON schema |
+| object | <code>Object</code> | object |
+
+**Example**  
+```js
+const isValid = skhema.isValid({
+	 type: 'object'
+}, {
+	 foo: 'bar'
+})
+
+if (isValid) {
+	 console.log('The object is valid')
+}
+```
+<a name="module_skhema.validate"></a>
+
+### skhema.validate(schema, object)
+**Kind**: static method of [<code>skhema</code>](#module_skhema)  
+**Summary**: Validate an object and throw if invalid  
+**Access**: public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| schema | <code>Object</code> | JSON schema |
+| object | <code>Object</code> | object |
+
+**Example**  
+```js
+skhema.validate({
+	 type: 'object'
+}, {
+	 foo: 'bar'
+})
+```
+<a name="module_skhema.merge"></a>
+
+### skhema.merge(schemas) ⇒ <code>Object</code>
+**Kind**: static method of [<code>skhema</code>](#module_skhema)  
+**Summary**: Merge two or more JSON Schemas  
+**Returns**: <code>Object</code> - merged JSON Schema  
+**Access**: public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| schemas | <code>Array.&lt;Object&gt;</code> | a set of JSON Schemas |
+
+**Example**  
+```js
+const result = skhema.merge([
+	 {
+		 type: 'string',
+		 maxLength: 5,
+		 minLength: 2
+	 },
+	 {
+		 type: 'string',
+		 maxLength: 3
+	 }
+])
+
+console.log(result)
+> {
+>	 type: 'string',
+>	 maxLength: 3,
+>	 minLength: 2
+> }
+```
+<a name="module_skhema.filter"></a>
+
+### skhema.filter(schema, object, [options]) ⇒ <code>Object</code> \| <code>Null</code>
+**Kind**: static method of [<code>skhema</code>](#module_skhema)  
+**Summary**: Filter an object based on a schema  
+**Returns**: <code>Object</code> \| <code>Null</code> - filtered object  
+**Access**: public  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| schema | <code>Object</code> |  | schema |
+| object | <code>Object</code> |  | object |
+| [options] | <code>Object</code> |  | options |
+| [options.force] | <code>Boolean</code> | <code>false</code> | force filter |
+
+**Example**  
+```js
+const result = skhema.filter({
+	 type: 'object',
+	 properties: {
+		 foo: {
+			 type: 'number'
+		 }
+	 },
+	 required: [ 'foo' ]
+}, {
+	 foo: 1,
+	 bar: 2
+})
+
+console.log(result)
+> {
+>	 foo: 1
+> }
+```
+
+Tests
+-----
+
+Run the test suite by doing:
+
+```sh
+$ npm test
+```
+
+Contribute
+----------
+
+We're looking forward to support more operating systems. Please raise an issue or even better, send a PR to increase support!
+
+- Issue Tracker: [github.com/resin-io-modules/skhema/issues](https://github.com/resin-io-modules/skhema/issues)
+- Source Code: [github.com/resin-io-modules/skhema](https://github.com/resin-io-modules/skhema)
+
+Before submitting a PR, please make sure that you include tests, and that the linter runs without any warning:
+
+```sh
+npm run lint
+```
+
+Support
+-------
+
+If you're having any problem, please [raise an issue](https://github.com/resin-io-modules/skhema/issues/new) on GitHub.
+
+License
+-------
+
+The project is licensed under the Apache 2.0 license.
---
+++ test/index.spec.js
@@ -0,0 +1,375 @@
+/*
+ * Copyright 2018 resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const _ = require('lodash')
+const ava = require('ava')
+const skhema = require('..')
+const MERGE_TEST_CASES = require('./merge.json')
+
+ava.test('.match() should validate a matching object', (test) => {
+	const result = skhema.match({
+		type: 'object'
+	}, {
+		foo: 'bar'
+	})
+
+	test.deepEqual(result, {
+		valid: true,
+		errors: []
+	})
+})
+
+ava.test('.match() should report back an error if no schema', (test) => {
+	const result = skhema.match(null, {
+		foo: 'bar'
+	})
+
+	test.deepEqual(result, {
+		valid: false,
+		errors: [
+			'no schema'
+		]
+	})
+})
+
+ava.test('.match() should report back a single error', (test) => {
+	const result = skhema.match({
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'number'
+			}
+		}
+	}, {
+		foo: 'bar'
+	})
+
+	test.deepEqual(result, {
+		valid: false,
+		errors: [
+			'data.foo should be number'
+		]
+	})
+})
+
+ava.test('.match() should report back more than one error', (test) => {
+	const result = skhema.match({
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'number'
+			},
+			bar: {
+				type: 'string'
+			}
+		},
+		required: [ 'foo', 'bar' ]
+	}, {
+		foo: 'bar'
+	})
+
+	test.deepEqual(result, {
+		valid: false,
+		errors: [
+			'data.foo should be number',
+			'data should have required property \'bar\''
+		]
+	})
+})
+
+ava.test('.match() should not match if the schema is not a valid schema', (test) => {
+	const result = skhema.match({
+		hello: 'foobar'
+	}, {
+		foo: 'bar'
+	})
+
+	test.deepEqual(result, {
+		valid: false,
+		errors: [
+			'invalid schema'
+		]
+	})
+})
+
+ava.test('.isValid() should return true if there is a match', (test) => {
+	const result = skhema.isValid({
+		type: 'object'
+	}, {
+		foo: 'bar'
+	})
+
+	test.true(result)
+})
+
+ava.test('.isValid() should return false if there is no match', (test) => {
+	const result = skhema.isValid({
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'number'
+			}
+		}
+	}, {
+		foo: 'bar'
+	})
+
+	test.false(result)
+})
+
+ava.test('.validate() should not throw if the object matches the schema', (test) => {
+	test.notThrows(() => {
+		skhema.validate({
+			type: 'object'
+		}, {
+			foo: 'bar'
+		})
+	})
+})
+
+ava.test('.validate() should throw if there is a single error', (test) => {
+	test.throws(() => {
+		skhema.validate({
+			type: 'object',
+			properties: {
+				foo: {
+					type: 'number'
+				}
+			}
+		}, {
+			foo: 'bar'
+		})
+	}, skhema.SchemaMismatch)
+})
+
+ava.test('.validate() should throw if there is more than one error', (test) => {
+	test.throws(() => {
+		skhema.validate({
+			type: 'object',
+			properties: {
+				foo: {
+					type: 'number'
+				},
+				bar: {
+					type: 'string'
+				}
+			},
+			required: [ 'foo', 'bar' ]
+		}, {
+			foo: 'bar'
+		})
+	}, skhema.SchemaMismatch)
+})
+
+ava.test('.filter() should remove additional properties from a top level object', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'number'
+			},
+			bar: {
+				type: 'string'
+			}
+		},
+		required: [ 'foo', 'bar' ]
+	}, {
+		foo: 1,
+		bar: 'foo',
+		baz: 'qux'
+	})
+
+	test.deepEqual(result, {
+		foo: 1,
+		bar: 'foo'
+	})
+})
+
+ava.test('.filter() should not remove properties given explicit additionalProperties', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		additionalProperties: true,
+		properties: {
+			foo: {
+				type: 'number'
+			},
+			bar: {
+				type: 'string'
+			}
+		},
+		required: [ 'foo', 'bar' ]
+	}, {
+		foo: 1,
+		bar: 'foo',
+		baz: 'qux'
+	})
+
+	test.deepEqual(result, {
+		foo: 1,
+		bar: 'foo',
+		baz: 'qux'
+	})
+})
+
+ava.test('.filter() should not remove properties given explicit additionalProperties and force: true', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		additionalProperties: true,
+		properties: {
+			foo: {
+				type: 'number'
+			},
+			bar: {
+				type: 'string'
+			}
+		},
+		required: [ 'foo', 'bar' ]
+	}, {
+		foo: 1,
+		bar: 'foo',
+		baz: 'qux'
+	}, {
+		force: true
+	})
+
+	test.deepEqual(result, {
+		foo: 1,
+		bar: 'foo'
+	})
+})
+
+ava.test('.filter() should return null if there is no match', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'number'
+			},
+			bar: {
+				type: 'string'
+			}
+		},
+		required: [ 'foo', 'bar' ]
+	}, {
+		foo: 'hello',
+		bar: 'foo',
+		baz: 'qux'
+	})
+
+	test.deepEqual(result, null)
+})
+
+ava.test('.filter() should remove additional properties from a nested object', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'number'
+			},
+			bar: {
+				type: 'object',
+				properties: {
+					baz: {
+						type: 'string'
+					}
+				},
+				required: [ 'baz' ]
+			}
+		},
+		required: [ 'foo', 'bar' ]
+	}, {
+		foo: 1,
+		bar: {
+			baz: 'hello',
+			qux: {
+				foo: 'bar'
+			}
+		},
+		baz: 'qux'
+	})
+
+	test.deepEqual(result, {
+		foo: 1,
+		bar: {
+			baz: 'hello'
+		}
+	})
+})
+
+ava.test('.filter() should correctly interpret fragments inside anyOf', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		anyOf: [
+			{
+				properties: {
+					foo: {
+						type: 'string'
+					}
+				}
+			}
+		]
+	}, {
+		foo: 'hello',
+		bar: 'foo',
+		baz: 'qux'
+	})
+
+	test.deepEqual(result, {
+		foo: 'hello'
+	})
+})
+
+ava.test('.filter() should correctly use top level properties when interpreting fragments inside anyOf', (test) => {
+	const result = skhema.filter({
+		type: 'object',
+		anyOf: [
+			{
+				properties: {
+					foo: {
+						type: 'string'
+					}
+				}
+			}
+		],
+		properties: {
+			bar: {
+				type: 'string'
+			}
+		}
+	}, {
+		foo: 'hello',
+		bar: 'foo',
+		baz: 'qux'
+	})
+
+	test.deepEqual(result, {
+		foo: 'hello',
+		bar: 'foo'
+	})
+})
+
+_.each(MERGE_TEST_CASES, (testCase, index) => {
+	ava.test(`.merge() should merge test case ${index}`, (test) => {
+		if (testCase.expected) {
+			const result = skhema.merge(testCase.schemas)
+			test.deepEqual(result, testCase.expected)
+		} else {
+			test.throws(() => {
+				skhema.merge(testCase.schemas)
+			}, skhema.IncompatibleSchemas)
+		}
+	})
+})
