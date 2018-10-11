3536e75a128f7b79d13727d3a5562df48e6f9d00
Lucian Buzzo <lucian.buzzo@gmail.com>
2018-10-11 07:56:26 +0100

    Validate against just the schema if `options.schemaOnly` is true
    Change-type: minor
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,31 @@
 import { JSONSchema6 } from 'json-schema';
 
-export interface FilterOptions {
-	// force no additional properties
+export interface MatchOptions {
+	// Custom formats to add validation for
+	customFormats?: {
+		[format: string]: (value: any) => boolean;
+	};
+	// Only validate the schema
+	schemaOnly?: boolean;
+	// Additional keywords to use (see https://github.com/epoberezkin/ajv-keywords)
+	keywords?: string[];
+}
+
+export interface FilterOptions extends MatchOptions {
+	// Force no additional properties
 	force: boolean;
 }
 
 export declare function merge(schema: Array<JSONSchema6 | null>): JSONSchema6;
 
-export declare function isValid(schema: JSONSchema6, value: any): boolean;
+export declare function isValid(schema: JSONSchema6, value: any, options?: MatchOptions): boolean;
 
-export declare function match(schema: JSONSchema6, value: any): {
+export declare function match(schema: JSONSchema6, value: any, options?: MatchOptions): {
 	valid: boolean;
 	errors: string[];
+	score: number;
 };
 
-export declare function validate(schema: JSONSchema6, value: any): void;
+export declare function validate(schema: JSONSchema6, value: any, options?: MatchOptions): void;
 
 export declare function filter<T = any>(schema: JSONSchema6, value: any, options?: FilterOptions): Partial<T> | null;
