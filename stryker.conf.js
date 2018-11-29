781b74d9ca847acfbfa3883fda66f4828824e3bc
Juan Cruz Viotti <juan@balena.io>
2018-11-29 23:59:40 +0000

    test: Configure Stryker for mutative testing
    Change-type: patch
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

---
+++ stryker.conf.js
@@ -0,0 +1,22 @@
+// See https://github.com/stryker-mutator/stryker/tree/master/packages/stryker#readme
+module.exports = (configuration) => {
+	configuration.set({
+		mutator: 'javascript',
+		packageManager: 'npm',
+		reporters: [ 'html', 'clear-text', 'progress' ],
+		testRunner: 'command',
+		commandRunner: {
+			command: './node_modules/.bin/ava --fail-fast'
+		},
+		transpilers: [],
+		logLevel: 'debug',
+		coverageAnalysis: 'off',
+		files: [
+			'package.json',
+			'lib/*.js',
+			'test/*.js',
+			'test/*.json'
+		],
+		mutate: [ 'lib/*.js' ]
+	})
+}
